ac5be68ddfa3cb202db5deb2d4c6f23b6141d80d
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-03-15 18:50:23 +0330

    add getting input from user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
     }
 }
